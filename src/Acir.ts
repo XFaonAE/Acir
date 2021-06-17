bf87a85ec33fab8374bc9678887ae03ad6c36da0
Rayyan <85759101+XfaonAE@users.noreply.github.com>
2021-06-17 00:54:21 -0400

    Added loader

--- src/Acir.ts
+++ src/Acir.ts
@@ -1,8 +1,59 @@
 const compilerConfig = require("./compiler.config");
 
 export class Acir {
-    constructor() {
+    loaderRunning: boolean = false;
+    loaderStateImage: string;
+    loaderStateNumber: number;
 
+    constructor() {
+        this.loader(true);
+        this.loaderProcess();
+        this.loaderStateNumber = 0;
+        this.loaderStateImage = "";
+    }
+
+    loader(start: boolean) {
+        this.loaderRunning = start;
+    }
+
+    loaderNextState() {
+        var states = [
+            "[ ============= ] |       1        ",
+            "[ #============ ] /       2        ",
+            "[ ==#========== ] -       3        ",
+            "[ ====#======== ] \\      4        ",
+            "[ ======#====== ] |       5        ",
+            "[ ========#==== ] /       6        ",
+            "[ ==========#== ] -       7        ",
+            "[ ============# ] \\      8        ",
+            "[ ============= ] |       10        ",
+            "[ ============# ] /       11        ",
+            "[ ==========#== ] -       12        ",
+            "[ ========#==== ] \\      13        ",
+            "[ ======#====== ] |       14        ",
+            "[ ====#======== ] /       15        ", 
+            "[ ==#========== ] -       16        ",
+            "[ #============ ] \\      17        "
+        ];
+
+        if (this.loaderStateNumber > states.length - 1) {
+            this.loaderStateNumber = 0;
+        }
+
+        this.loaderStateNumber++;
+
+        this.loaderStateImage = states[this.loaderStateNumber - 1];
+    }
+
+    loaderProcess() {
+        setInterval(() => {
+            this.loaderNextState();
+            process.stdout.write(this.loaderStateImage + "\r");
+        }, 50);
+    }
+
+    printError(message: string) {
+        process.stdout.write("[ Error ] " + message);
     }
 
     run() {
